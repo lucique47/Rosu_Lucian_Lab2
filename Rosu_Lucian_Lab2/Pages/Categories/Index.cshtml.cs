5add5cef51febfecf773243bbea1e2da98558209
LUCIAN-DAVID ROȘU <lucian.rosu@stud.ubbcluj.ro>
2024-12-15 19:27:23 +0200

    Laborator4

--- Rosu_Lucian_Lab2/Pages/Categories/Index.cshtml.cs
+++ Rosu_Lucian_Lab2/Pages/Categories/Index.cshtml.cs
@@ -7,23 +7,41 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Rosu_Lucian_Lab2.Data;
 using Rosu_Lucian_Lab2.Models;
+using Rosu_Lucian_Lab2.Models.ViewModels;
+
 
 namespace Rosu_Lucian_Lab2.Pages.Categories
 {
     public class IndexModel : PageModel
     {
-        private readonly Rosu_Lucian_Lab2.Data.Rosu_Lucian_Lab2Context _context;
+        private readonly Rosu_Lucian_Lab2Context _context;
 
-        public IndexModel(Rosu_Lucian_Lab2.Data.Rosu_Lucian_Lab2Context context)
+        public IndexModel(Rosu_Lucian_Lab2Context context)
         {
             _context = context;
         }
 
-        public IList<Category> Category { get;set; } = default!;
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+                .Include(c => c.BookCategories)
+                    .ThenInclude(bc => bc.Book)
+                        .ThenInclude(b => b.Author)
+                .OrderBy(c => c.CategoryName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                    .Where(c => c.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories.Select(bc => bc.Book);
+            }
         }
     }
 }
