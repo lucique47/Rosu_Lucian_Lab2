c450131eba2272bd54bb6f78c1acd8d41921de04
PC <PC@LUCIPC>
2024-12-15 14:18:42 +0200

    Add project files.

---
+++ Rosu_Lucian_Lab2/Pages/Authors/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Rosu_Lucian_Lab2.Data;
+using Rosu_Lucian_Lab2.Models;
+
+namespace Rosu_Lucian_Lab2.Pages.Authors
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Rosu_Lucian_Lab2.Data.Rosu_Lucian_Lab2Context _context;
+
+        public DeleteModel(Rosu_Lucian_Lab2.Data.Rosu_Lucian_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Author Authors { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var authors = await _context.Authors.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (authors == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Authors = authors;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var authors = await _context.Authors.FindAsync(id);
+            if (authors != null)
+            {
+                Authors = authors;
+                _context.Authors.Remove(Authors);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
